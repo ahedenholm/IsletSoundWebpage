2df4c24a209015efd013dee0b19bbdddfb9ffbe9
Anders Hedenholm <bocheadphase@gmail.com>
2018-09-01 14:14:02 +0200

    initial commit

---
+++ src/components/currentProjects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CurrentProjects extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CurrentProjects;
