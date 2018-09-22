3635ed55beabf3078d66b9a32c56ae8d3b9cdc58
Anders HEDENHOLM <anders.hedenholm@soprasteria.com>
2018-09-23 00:38:10 +0200

    Added skeleton UI framwork, various changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+import Contact from './components/Contact';
+import CurrentProjects from './components/CurrentProjects';
+import LandingPage from './components/LandingPage';
+import Listen from './components/Listen';
+import NavigationBar from './components/NavigationBar';
+
+const 
+  INTRO = 'INTRO',
+  LISTEN = 'LISTEN',
+  CURRENT = 'CURRENT',
+  CONTACT = 'CONTACT';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeContent: INTRO,
+    }
+  }
+  toggleActiveContent = (content) => {
+    this.setState({ activeContent : content });
+  }
   render() {
     return (
-      <div className="bg padding24px">
-        Hello
+      <div class="container">
+        <section className="displayFlex flexCenter flexColumn marginTop150px">
+          <h2>Islet Sound</h2>
+          <h4>Music production for games</h4>
+          <div>
+            <button onClick={()=> this.toggleActiveContent(LISTEN)} className={(this.state.activeContent === LISTEN ? "button-primary" : "button") + " button margin2px" }>LISTEN</button>
+            <button onClick={()=> this.toggleActiveContent(CURRENT)} className={(this.state.activeContent === CURRENT ? "button-primary" : "button") + " button margin2px"}>CURRENT PROJECTS</button>
+            <button onClick={()=> this.toggleActiveContent(CONTACT)} className={(this.state.activeContent === CONTACT ? "button-primary" : "button") + " button margin2px"}> CONTACT</button>
+          </div>
+          <Listen />
+        </section>
       </div>
     );
   }
--- src/components/listen.js
+++ src/components/listen.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Listen extends Component {
   render() {
     return (
-      <div>
+      <div className="animFadeIn width50vw">
+        <div className="paddingBottom35px paddingTop35px">
+          <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/185023185&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+        </div>
+        <div className="paddingBottom35px">
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/34489385&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+        </div>
+        <div className="paddingBottom35px">
+          <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/166946741&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+        </div>
       </div>
     );
   }
